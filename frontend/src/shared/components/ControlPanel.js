522752996be38bf3d23ba7e46393331a61f4401a
Andrew Cao <ancao@Andrews-MacBook-Pro.local>
2022-01-04 18:41:14 +1100

    Updated controls for weight page

--- frontend/src/shared/components/ControlPanel.js
+++ frontend/src/shared/components/ControlPanel.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import DatePicker from './DatePicker';
 
 import './ControlPanel.css'
 const ControlPanel = () => {
-    
+    const [datePrecision, setDatePrecision] = React.useState('left');
+
+    const handleDatePrecision = (event, newDatePrecision) => {
+      setDatePrecision(newDatePrecision);
+    };
+
     return (
         <div className='control-panel'>
             <Grid container spacing={2}>
@@ -13,6 +18,28 @@ const ControlPanel = () => {
                 </Grid>
                 <Grid item xs={6}>
                     <DatePicker title='To'/>
+                </Grid>
+                <Grid item xs={12}>
+                    <ToggleButtonGroup
+                    exclusive
+                    id="datePrecision"
+                    name="datePrecision"
+                    className='date-precision'
+                    type="text"
+                    fullWidth
+                    onChange={handleDatePrecision}
+                    value={datePrecision}
+                    >
+                        <ToggleButton value="Daily" aria-label="left aligned">
+                            Daily
+                        </ToggleButton>
+                        <ToggleButton value="Weekly" aria-label="centered">
+                            Weekly
+                        </ToggleButton>
+                        <ToggleButton value="Monthly" aria-label="right aligned">
+                            Monthly
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Grid>
             </Grid>
         </div>
